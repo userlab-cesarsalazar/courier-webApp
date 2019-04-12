5c4c824bcab22ee23c66ffb1a383f501fa3c5049
Felix Fermin <felixfermin7@gmail.com>
2019-04-12 14:33:39 -0600

    Layout, menu and initial structure (#1)

---
+++ src/sections/packages/PackagesPage.js
@@ -0,0 +1,32 @@
+//Libs
+import React, { Component } from 'react';
+
+//Api
+
+//Components
+
+
+//Styles
+
+//const
+
+
+class PackagesPage extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Packages</h2>
+
+      </div>
+    );
+  }
+}
+
+export default PackagesPage
