24c5f2f068744e29bd69cf79eff8dcdcf500f212
cesar <cesar.augs@gmail.com>
2019-06-05 11:29:51 -0600

    - add report services add new fields

--- src/sections/reports/ReportsPage.js
+++ src/sections/reports/ReportsPage.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 
 //Api
-import ClientsSrc from "../clients/ClientsSrc";
+import ReportSrc from "../reports/ReportsSrc";
 
 //Components
 import UIDateSelect from '../../commons/components/UIDateSelect'
 import UIIntegerInput from '../../commons/components/UIIntegerInput'
 import ClientSearchSelect from '../clients/components/ClientSearchSelect'
+import { DatePicker } from 'antd';
 
 import {
   Form,
@@ -33,11 +34,29 @@ class ReportsPage extends Component {
     this.onSearch = this.onSearch.bind(this)
     this.state = {
       radioValue: 'today',
-      dateRange: null
+      dateRange: null,
+      type: 'CIERRE'
     }
   }
 
-  onSearch = () => {}
+  onSearch = () => {
+    console.log(this.state,'search')
+    let params = {
+      total: this.state.type === 'CIERRE' ? true : false,
+      date: this.state.date.format('YYYY-MM-DD')
+    }
+    
+    console.log(params,'params')
+    
+    ReportSrc.totals(params)
+      .then( result => {
+        console.log( result,'result')
+      })
+      .catch( e => {
+      
+      })
+    
+  }
 
   handleChange = (name, value) => {
 
@@ -55,10 +74,10 @@ class ReportsPage extends Component {
       }
     }
 
-    this.setState({ [name]: value, ...otherState })
+    this.setState({ [name]: value, ...otherState },_ => { console.log(this.state,'state')})
   }
 
-
+/*
   searchClient = async client_id => {
 
     try {
@@ -75,7 +94,7 @@ class ReportsPage extends Component {
       this.setState({ clientLoading: false });
       message.error(e);
     }
-  }
+  }*/
 
 
   render() {
@@ -83,7 +102,9 @@ class ReportsPage extends Component {
     const {
       dateRange,
       client_id,
-      client
+      client,
+      type,
+      date
     } = this.state
 
     return (
@@ -91,57 +112,80 @@ class ReportsPage extends Component {
         <Card title='Reportes' style={{ width: '100%' }}>
           <Form>
             <Row>
-              <Col span={24}>
-                <FormItem label='Fecha' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                  <UIDateSelect
-                    onChange={value => this.handleChange('dateRange', value)}
-                    value={dateRange}
-                    placeholder={'Fecha'}
-                  />
-                </FormItem>
-              </Col>
               <Col span={12}>
-                <FormItem label='Cod. Cliente' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                  <UIIntegerInput
-                    onBlur={this.handleBlur}
-                    onChange={e => this.handleChange('client_id', e.target.value)}
-                    value={client_id}
-                    placeholder='Cod. Cliente'
-                  />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label='Nombre Cliente' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                  <ClientSearchSelect
-                    value={client}
-                    onChange={value => this.handleChange('client', value)}
-                  />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label='Nro. Tracking' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                  <Input placeholder={'Nro. Tracking'} />
-                </FormItem>
-              </Col>
-              <Col span={12}>
-                <FormItem label='Estado' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
-                  <Select placeholder='Seleccione'>
-                    <Option value='registrado'>Registrado</Option>
-                    <Option value='transito'>En transito guatemala</Option>
-                    <Option value='recibidofi'>Recibido en oficina</Option>
-                    <Option value='ruta'>En ruta</Option>
-                    <Option value='entregado'>Entregado</Option>
-                    <Option value='tba'>TBA</Option>
+                <FormItem label='Tipo de Reporte' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                  <Select placeholder='Seleccione' onChange={value => this.handleChange('type', value)} value={type} defaultValue={type}>
+                    <Option value='GENERAL'>General</Option>
+                    <Option value='CIERRE'>Cierre</Option>
                   </Select>
                 </FormItem>
               </Col>
+              { this.state.type === 'GENERAL' &&
+              <div>
+                <Col span={12}>
+                  <FormItem label='Fecha' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <UIDateSelect
+                      onChange={value => this.handleChange('dateRange', value)}
+                      value={dateRange}
+                      placeholder={'Fecha'}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label='Cod. Cliente' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <UIIntegerInput
+                      onBlur={this.handleBlur}
+                      onChange={e => this.handleChange('client_id', e.target.value)}
+                      value={client_id}
+                      placeholder='Cod. Cliente'
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label='Nombre Cliente' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <ClientSearchSelect
+                      value={client}
+                      onChange={value => this.handleChange('client', value)}
+                    />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label='Nro. Tracking' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <Input placeholder={'Nro. Tracking'} />
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label='Estado' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <Select placeholder='Seleccione'>
+                      <Option value='registrado'>Registrado</Option>
+                      <Option value='transito'>En transito guatemala</Option>
+                      <Option value='recibidofi'>Recibido en oficina</Option>
+                      <Option value='ruta'>En ruta</Option>
+                      <Option value='entregado'>Entregado</Option>
+                      <Option value='tba'>TBA</Option>
+                    </Select>
+                  </FormItem>
+                </Col>
+              </div>
+              }
+              
+              { this.state.type === 'CIERRE' &&
+                <Col span={12}>
+                  <FormItem label='Fecha' labelCol={{ span: 6 }} wrapperCol={{ span: 12 }}>
+                    <DatePicker placeholder="Seleccione una Fecha"
+                                onChange={value => this.handleChange('date', value)}
+                                value={date} style={{ width: '300px'}} />
+                  </FormItem>
+                </Col>
+              }
             </Row>
-
-            <FormItem wrapperCol={{ span: 6, offset: 9 }}>
-              <Button type='primary' onClick={this.onSearch}>
-                Buscar
-              </Button>
-            </FormItem>
+            <div style={{textAlign:'center'}}>
+              <FormItem>
+                <Button type='primary' onClick={this.onSearch}>
+                  Buscar
+                </Button>
+              </FormItem>
+            </div>
           </Form>
         </Card>
       </div>
---
+++ src/sections/reports/ReportsSrc.js
@@ -0,0 +1,11 @@
+import api from '../../config/api'
+import { stage } from '../../config/credentials'
+
+const url = stage.reportURL;
+const totals = (params) => api.get(`${url}?date='${params.date}'&total=${params.total}`);
+const closedReport = (params) => api.get(url+'?type=cliente'+params);
+
+
+export default {
+  totals,
+}
